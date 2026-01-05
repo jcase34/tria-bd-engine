81fda203abe89d0ce7f6d17ffcf0776de462c1ea
Jacob Case <jacobcase@DevBook.local>
2026-01-05 12:17:42 -0700

    feat: implement CORS headers, testable API call for hello world

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client"
+
 import Image from "next/image";
 
 export default function Home() {
+
+  async function getDjangoAPIData() {
+    const response = await fetch("http://127.0.0.1:8000/api/hello");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async function handleClick() {
+    await getDjangoAPIData();
+  }
+  
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -12,6 +25,9 @@ export default function Home() {
           height={20}
           priority
         />
+        <button onClick={handleClick}>
+          Lookup Data
+        </button>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
